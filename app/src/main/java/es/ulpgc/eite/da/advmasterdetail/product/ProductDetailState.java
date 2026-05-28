fb91dccb919b414b4bdb98fdd0a8ea306dcb1cf9
Arlyn <arlyngc2001@gmail.com>
2026-05-28 12:54:12 +0100

    Initial project setup from AdvMasterDetail base

---
+++ app/src/main/java/es/ulpgc/eite/da/advmasterdetail/product/ProductDetailState.java
@@ -0,0 +1,8 @@
+package es.ulpgc.eite.da.advmasterdetail.product;
+
+import es.ulpgc.eite.da.advmasterdetail.data.CategoryItem;
+
+public class ProductDetailState extends ProductDetailViewModel {
+
+    public CategoryItem category;
+}
